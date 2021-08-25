632c406b1f50475d1fd624e998a9ded4be59c2e6
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-26 01:32:11 +0700

    Add files via upload

---
+++ Penamaan Konstanta.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+/*
+NAMA    : RIZKI RAMADHANI
+JUDUL   : Penamaan Konstanta
+BAHASA  : C++
+*/
+
+using namespace std;
+
+const string Kampus = "Massachusetts Institute of Technology";
+const double IPK = 3.99;
+
+int main()
+{
+  const string Kampus = "Massachusetts Institute of Technology";
+  const double IPK = 3.99;
+
+  cout <<"=========================================================== \n";
+  cout <<"================== Contoh Status Sosmed =================== \n";
+  cout <<"=========================================================== \n";
+  cout << "\n";
+  cout << "Mahasiswa Jurusan IT Di " << Kampus;
+  cout << "\n";
+  cout << "Semoga Mendapatkan IPK " << IPK;
+  cout << "\n";
+
+  return 0;
+}
