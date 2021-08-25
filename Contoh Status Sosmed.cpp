f3b7eeadac4f4433a3f01ea8cbb886477a1c6331
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-26 01:22:23 +0700

    Penamaan Konstanta

---
+++ Contoh Status Sosmed.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/*
+NAMA    : RIZKI RAMADHANI
+JUDUL   : Contoh Status Sosmed
+BAHASA  : C++
+*/
+
+using namespace std;
+
+#define Kampus "Massachusetts Institute of Technology"
+#define ipk 3.99
+
+int main()
+{
+  cout <<"=========================================================== \n";
+  cout <<"================== Contoh Status Sosmed =================== \n";
+  cout <<"=========================================================== \n";
+  cout << "\n";
+  cout << "Mahasiswa Jurusan IT Di " << Kampus;
+  cout << "\n";
+
+  cout << "Semoga Mendapatkan IPK " << ipk;
+  cout << "\n";
+
+  return 0;
+}
