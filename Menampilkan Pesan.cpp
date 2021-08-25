7c96b884001ca145a6fe499c09e89cce3b93640d
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-26 00:14:14 +0700

    Add files via upload

---
+++ Menampilkan Pesan.cpp
@@ -0,0 +1,13 @@
+// Pembuat : Rizki Ramadhani
+// Tujuan  : Menampilkan pesan menggunakan C++
+// Bahasa  : C++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Belajar C++ Baru \n";
+  return 0;
+}
