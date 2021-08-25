a914664d9c50ca6968736d500cd7454d73bd3805
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-26 01:13:23 +0700

    Add files via upload

---
+++ Luas Persegi Panjang.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/*
+NAMA    : RIZKI RAMADHANI
+JUDUL   : Luas Persegi Panjang
+BAHASA  : C++
+*/
+
+using namespace std;
+
+#define PANJANG 8
+#define LEBAR 5
+
+int main ()
+{
+  cout <<"=========================================================== \n";
+  cout <<"================== Luas Persegi Panjang =================== \n";
+  cout <<"=========================================================== \n";
+
+  cout << "Panjang = " << PANJANG;
+  cout << "\n";
+
+  cout << "Lebar = " << LEBAR;
+  cout << "\n";
+
+  cout << "Luas Persegi Panjang Adalah = " << PANJANG*LEBAR;
+  cout << "\n";
+
+  return 0;
+}
